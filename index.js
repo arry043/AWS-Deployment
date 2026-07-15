d258b0ba022c22df522c63fde93aa62db9c91ea2
Arry <arifquerry@gmail.com>
2026-07-15 13:11:40 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,18 @@
+import express from "express"
+import dotenv from "dotenv"
+dotenv.config()
+
+const port = process.env.PORT || 5000
+const app = express()
+
+app.get("/health", (req, res) => {
+    return res.status(200).json({ message: "Server is healthy" })
+})
+
+app.get("/", (req, res) => {
+    return res.status(200).json({ message: "Hello from AWS Cloud" })
+})
+
+app.listen(port, () => {
+    console.log(`Server Started at ${port}`)
+})
